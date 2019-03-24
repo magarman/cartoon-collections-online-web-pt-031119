b225a6a7947c3a11010ba0b471c87e4d79546485
Mari Garman <garman.mariana@gmail.com>
2019-03-24 19:35:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,16 @@ def long_planeteer_calls(calls)
   calls.any? {|calls| calls.size > 4}
 end
 
+def find_the_cheese(potentially_cheesy_items)
+  cheeses = %w(gouda cheddar camembert)
+
+  potentially_cheesy_items.find do |maybe_cheese|
+    cheeses.include?(maybe_cheese)
+  end
+end
 
 def find_the_cheese(array)
-  if array.include?(cheddar)
-else no_cheese 
+  bunchacheese = ("gouda", "cheddar", "camembert")
+  array.find {|cheese| bunchacheese.include?(cheese) }
+ end
 end
