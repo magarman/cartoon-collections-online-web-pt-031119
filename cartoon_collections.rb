9f1b97a7c7c423ba4deb03cd29106fdc252c3fed
Mari Garman <garman.mariana@gmail.com>
2019-03-24 19:18:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,8 +13,6 @@ end
 
 
 def find_the_cheese(array)
-  array.include?(cheese_tpes)
-  
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+  if array.include?(cheese_types) 
+    "#{cheese_types}"
 end
