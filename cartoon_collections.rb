a4fa3a51d8d0d374bab00409baa95197095145b5
Mari Garman <garman.mariana@gmail.com>
2019-03-24 20:10:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(array)
-  bunchacheese = ["gouda", "cheddar", "camembert"]
+  bunchacheese = (cheddar gouda camembert)
   array.find {|cheese| bunchacheese.include?(cheese)}
  end
 end
